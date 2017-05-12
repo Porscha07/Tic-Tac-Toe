48d903d04e6227d2d2e0de8374fecb101c44079e
Porscha07 <porscha.uga@gmail.com>
2017-05-11 20:44:12 -0400

    need to work on layout,add audio,other pictures/fix background

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,6 @@
 // 1. Check-Set up Board.
 //2. User should be able to click on a box and mark the square (with users mark.) can put an oncllick directly on the square.
-//addeventlistener
+//addeventlistener is the JS equavilant to onclick.
 //create a marksquare function
 //3. If its X turn put X in, if its O turn, put O in. ( keep track of whos turn it is)
 //4. now that we know whos turn it is, when markSquare gets called put their symbol in and change whos turn it is. 
@@ -23,23 +23,27 @@ var winningCombos = [
 var gameOverBool = false;
 var onePlayerGame = true;
 
+// ***************USER IS ABLE TO CLICK ON A BOX & MARK THE SQUARE******************************************************* 
 var squares = document.getElementsByClassName('square');
+// var britney = document.createElement("britney-icon.jpg");
+// let allows you to declare variables that are limited in scope to the block, statement, or expression on which it is used. This is unlike the var keyword, which defines a variable globally, or locally to an entire function regardless of block scope.
 for(let i = 0; i < squares.length; i++){
 	//console.log(squares[i]);
 	squares[i].addEventListener('click',function(event){
 	// console.log("User clicked on a square!")
-		if(!gameOverBool){
+		if(!gameOverBool){ //if the game over is true. (then will run markSquare function)the user is able to click on a square. then we are going to run the Marskquare funtion and call the paasesd up name- currentSquare.)
 			markSquare(this)
 		}
 	});
 }
-// create a markSquare function
+// ***************************create a markSquare function***********************************************
 
 function markSquare(currentSquare){
 	console.log(currentSquare.id);
 	var squareResult = ""
+	// var britney = document.createElement("britney-icon.jpg");
 	// console.log(currentSquare.innerHTML)
-	if((currentSquare.innerHTML == "X") || (currentSquare.innerHTML == "O")){
+	if((currentSquare.innerHTML == "X" || (currentSquare.innerHTML == "O")){
 		// console.log("This square is taken")
 		squareResult = "Sorry, this square is taken."
 	}else if (gameOverBool){
@@ -71,6 +75,7 @@ function computerMove(){
 	// if it is, send it a square,
 	// 	if it's not, keep looking
 }
+// ************************** Check win to see if the current player won/also breaks out of the amain loop*****************************************************
 
 function checkWin(currentPlayersSquares,whoJustWent){
 	// outter loop ( winning combos)
@@ -92,19 +97,22 @@ function checkWin(currentPlayersSquares,whoJustWent){
 				console.log("player" + whoJustWent + "won the game!");
 				// stop checking i's the game is over....
 				gameOver(whoJustWent,winningCombos[i]);
-			}	break;
+				break;
+			}
 
 		}
 
 	}
+// /***************************Loop for determining when the game is over and how you won****************************************************
 
 function gameOver(whoJustWon,winningCombo){
 	var messageElement = document.getElementById('message');
 	var message = "Congratulations to player" + whoJustWon + ".You won with" + winningCombo;
+	console.log(winningCombo);
 	document.getElementById('message').innerHTML = message;
 	// console.log(message);
 	// console.dir(messageELement)
-	messageELement.innerHTML = message;
+	messageElement.innerHTML = message;
 	// console.dir(messageELement)
 	for(let i = 0; i < winningCombo.length; i++){
 		document.getElementById(winningCombo[i]).className += " winning-square";
